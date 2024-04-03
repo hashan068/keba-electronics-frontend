a213e6cdcddb9a30f6254732f872729f12121cab
Hashan Ruchira <hashanruchira98@gmail.com>
2024-04-03 13:28:37 +0530

    Refactor LoadingIndicator and Product components

--- frontend/src/components/LoadingIndicator.jsx
+++ frontend/src/components/LoadingIndicator.jsx
@@ -8,3 +8,4 @@ const LoadingIndicator = () => {
 }
 
 export default LoadingIndicator
+
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,22 +1,68 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import PermanentDrawerLeft from '../components/PermanentDrawerLeft';
+import api from "../api";
 
 export default function Product() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    api
+        .get("/api/sales/products/")
+        .then((res) => {
+            console.log(res.data);
+            return res.data;
+        })
+        .then((data) => {
+            setProducts(data);
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            alert(err);
+        });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return(
     <Box sx={{ flexGrow: 1 }}>
-      <PermanentDrawerLeft/>
-      <Box component='main' sx={{ flexGrow: 1 }}>
+      <PermanentDrawerLeft />
 
-
-        <Typography variant='h3' align='center' sx={{ marginTop: '55px' }}> 
-          Welcome to Products
+      <Box component="main" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '70%' }}>
+        <Typography variant="h3" align="center" sx={{ marginTop: "55px" }}>
+          Products
         </Typography>
         
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+
+            <TableHead>
+              <TableRow>
+                <TableCell>Product Name</TableCell>
+                <TableCell align="right">Description</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {products.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell component="th" scope="row">
+                    {product.name}
+                  </TableCell>
+                  <TableCell align="right">{product.description}</TableCell>
+                  <TableCell align="right">{product.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+
+          </Table>
+        </TableContainer>
+
       </Box>
     </Box>
-  ) 
+  )
 };
-
-
-
